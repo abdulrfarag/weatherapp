ee1ead69fe880cdb6f7612b991893125b0c1bfbb
abdulrfarag <ar060290@gmail.com>
2022-09-15 21:11:49 -0500

    basic setup of html

--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,4 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b1bfaa8aef920555a5427b626bedb8c9")
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.885&lon=-87.626&appid=b1bfaa8aef920555a5427b626bedb8c9")
 .then(function(response){
     return response.json()
 })
